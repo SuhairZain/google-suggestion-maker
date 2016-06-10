973e9953054fdfe3654517e1524aeb35acce6de1
SuhairZain <suhairzain11@gmail.com>
2016-06-11 01:48:13 +0530

    Moved google font spec to seperate file

---
+++ src/utils/google-specific.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Suhair Zain on 11/6/16.
+ */
+
+export const addGoogleFontSpec = function(style){
+    return {
+        ...style,
+        font: '16px arial,sans-serif'
+    };
+};
