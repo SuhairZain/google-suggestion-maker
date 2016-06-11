e0a1e92a148b5e7a1fdf926a0c49fed6ae3629f9
SuhairZain <suhairzain11@gmail.com>
2016-06-11 14:34:35 +0530

    Added suggestion items

--- src/SearchInputs.jsx
+++ src/SearchInputs.jsx
@@ -4,20 +4,68 @@
 
 import React, {Component} from 'react';
 
+import SearchInputItem from './SearchInputItem.jsx';
+import SuggestionInputItem from './SuggestionInputItem.jsx';
+
 class SearchInputs extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            searchText: 'how to ',
+            suggestions: ['be single', 'be awesome']
+        };
+    }
+
     styles = {
         root: {
-            alignItems: 'center',
+            display: 'flex',
+            width: '50%'
+        },
+        fields: {
             display: 'flex',
             flexDirection: 'column',
-            width: '50%'
+            width: '100%'
         }
     };
 
+    handleSearchChange = (evt) => {
+        this.setState({
+            ...this.state,
+            searchText: evt.target.value
+        });
+    };
+
+    handleSuggestionChange = (suggestion, index) => {
+        const suggestions = this.state.suggestions;
+        suggestions[index] = suggestion;
+        this.setState({
+            ...this.state,
+            suggestions: suggestions
+        });
+    };
+
     render() {
+        const self = this;
+        var i = 0;
+
         return (
             <div style={this.styles.root}>
-                Inputs
+                <div style={this.styles.fields}>
+                    <SearchInputItem
+                        text={this.state.searchText}
+                        label="Enter the search here"
+                        onChange={this.handleSearchChange}/>
+                    {this.state.suggestions.map(function(suggestion){
+                        return (
+                        <SuggestionInputItem
+                            suggestion={suggestion}
+                            label="Enter a suggestion here"
+                            index={i++}
+                            key={i}
+                            onChange={self.handleSuggestionChange}/>
+                        )
+                    })}
+                </div>
             </div>
         );
     }
---
+++ src/SuggestionInputItem.jsx
@@ -0,0 +1,23 @@
+/**
+ * Created by Suhair Zain on 11/6/16.
+ */
+
+import React, {Component} from 'react';
+
+import SearchInputItem from './SearchInputItem.jsx';
+
+const styles = {};
+
+class SuggestionInputItem extends React.Component {
+    handleChange = (evt) => {
+        this.props.onChange(evt.target.value, this.props.index);
+    };
+
+    render() {
+        return (
+            <SearchInputItem text={this.props.suggestion} label="Enter a suggestion here" onChange={this.handleChange}/>
+        );
+    }
+}
+
+export default SuggestionInputItem;
